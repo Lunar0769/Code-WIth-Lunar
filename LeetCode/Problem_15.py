07c3a435504637c32bb763bc15c84e86d5d67f34
Lunar0769 <pkavya085@gmail.com>
2026-07-20 17:20:21 +0530

    Problem_16 Leetcode

--- LeetCode/Problem_15.py
+++ LeetCode/Problem_15.py
@@ -37,19 +37,26 @@
 # 3 <= nums.length <= 3000
 # -105 <= nums[i] <= 105
 
-class Solution(object):
-    def threeSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        ans = []
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                for k in range(j + 1, len(nums)):
-                    if nums[i] + nums[j] + nums[k] == 0:
-                        a = ([nums[i], nums[j], nums[k]])
-                        a.sort()
-                        if a not in ans:
-                            ans.append(a)
-        return ans
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s < 0:
+                    l += 1
+                elif s > 0:
+                    r -= 1
+                else:
+                    res.append((nums[i], nums[l], nums[r]))
+                    while l < r and nums[l] == nums[l + 1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r - 1]:
+                        r -= 1
+                    l += 1
+                    r -= 1
+        return res
